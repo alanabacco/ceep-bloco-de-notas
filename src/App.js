424b0f189c8c942c5823ae8f7463d491086e232c
Alana Bacco <alana_bacco@hotmail.com>
2021-02-16 21:37:08 -0300

    final da terceira aula

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import FormularioCadastro from './components/formularioCadastro';
-import ListaDeNotas from "./components/ListaDeNotas"
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas/";
+import FormularioCadastro from "./components/FormularioCadastro/";
+import "./assets/App.css";
+import './assets/index.css';
 
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
-      </section >
+      </section>
     );
   }
 }
