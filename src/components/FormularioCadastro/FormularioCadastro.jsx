d7c30bf95ded68b005e7edeb20cde403afa48994
Alana Bacco <alana_bacco@hotmail.com>
2021-02-20 18:32:28 -0300

    final da quarta aula

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import "./style.css";
 
 class FormularioCadastro extends Component {
+
+  constructor() {
+    super()
+    this.titulo = "";
+  }
+
+  handleMudancaTitulo(evento){
+    this.titulo = evento.target.value
+  }
+
   render() {
     return (
       <form className="form-cadastro ">
+        <label>Título:</label>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
+        <br />
+        <label>Escreva sua nota:</label>
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
